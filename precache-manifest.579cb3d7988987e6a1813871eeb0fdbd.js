58a7f1daf3ddf52f2c4ca2522cfd871a65e052cf
kulix <kulixon261@gmail.com>
2023-10-26 12:13:27 +0200

    Updates

---
+++ precache-manifest.579cb3d7988987e6a1813871eeb0fdbd.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28fe4bdc2ea20a9e5c5e76902a656ac2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1293d12f987dfa993805",
+    "url": "/Portfolio/static/css/2.24075002.chunk.css"
+  },
+  {
+    "revision": "921a5eae600ef9f27878",
+    "url": "/Portfolio/static/css/main.37e560d4.chunk.css"
+  },
+  {
+    "revision": "1293d12f987dfa993805",
+    "url": "/Portfolio/static/js/2.4e1c05f5.chunk.js"
+  },
+  {
+    "revision": "250dfb561c4f76bbfd0e503699e8577c",
+    "url": "/Portfolio/static/js/2.4e1c05f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "921a5eae600ef9f27878",
+    "url": "/Portfolio/static/js/main.93ceafde.chunk.js"
+  },
+  {
+    "revision": "db1ea67b8a08076c9f67",
+    "url": "/Portfolio/static/js/runtime-main.0d26a884.js"
+  },
+  {
+    "revision": "ad1f0c04190c3a676491a7a98acdc3db",
+    "url": "/Portfolio/static/media/cloon-thumbnail.ad1f0c04.webp"
+  },
+  {
+    "revision": "62eab3f478ea952fe37f67d89b31eb11",
+    "url": "/Portfolio/static/media/cloon1.62eab3f4.webp"
+  },
+  {
+    "revision": "4f459c937dcd153f5ee67c22ceed0f49",
+    "url": "/Portfolio/static/media/cloon2.4f459c93.webp"
+  },
+  {
+    "revision": "37ec52cca3974f7355cad3e3ee7fc119",
+    "url": "/Portfolio/static/media/cloon3.37ec52cc.webp"
+  },
+  {
+    "revision": "c56d9017cbafe02afe214505a51dbc64",
+    "url": "/Portfolio/static/media/clotify-thumbnail.c56d9017.webp"
+  },
+  {
+    "revision": "8916d6a9451cd2373c670f8c4b073ef3",
+    "url": "/Portfolio/static/media/clotify1.8916d6a9.webp"
+  },
+  {
+    "revision": "388e89808de0430697c26ead2772a8bb",
+    "url": "/Portfolio/static/media/contact-splash.388e8980.svg"
+  },
+  {
+    "revision": "8dc69cc7f93c8dd87a6cceafd793c419",
+    "url": "/Portfolio/static/media/cta-splash.8dc69cc7.svg"
+  },
+  {
+    "revision": "97f614b6dc03bf56dd168de1025183db",
+    "url": "/Portfolio/static/media/mangaplanet-thumbnail.97f614b6.webp"
+  },
+  {
+    "revision": "c4c9555bbef9120a86efdfeab3e9aaf7",
+    "url": "/Portfolio/static/media/mangaplanet1.c4c9555b.webp"
+  },
+  {
+    "revision": "a735ae20df0d50cc5511f990cd7a8f4e",
+    "url": "/Portfolio/static/media/mangaplanet2.a735ae20.webp"
+  },
+  {
+    "revision": "5c0ee9c452232a52f7d06fdddb37ecb8",
+    "url": "/Portfolio/static/media/mangaplanet3.5c0ee9c4.webp"
+  },
+  {
+    "revision": "ebab7f14272b2e532a9552fa6e665f45",
+    "url": "/Portfolio/static/media/mangaplanet4.ebab7f14.webp"
+  },
+  {
+    "revision": "cb67c3e11b38aa12b5c1c183aaa2fb3f",
+    "url": "/Portfolio/static/media/me.cb67c3e1.svg"
+  },
+  {
+    "revision": "5ade5e2878a050f1e59d37a8e424c838",
+    "url": "/Portfolio/static/media/nodejs.5ade5e28.png"
+  },
+  {
+    "revision": "9039a69d7f22c6b185f37544e75187a7",
+    "url": "/Portfolio/static/media/pacman-thumbnail.9039a69d.webp"
+  },
+  {
+    "revision": "728a84e97882574b40cf13c391ab4e42",
+    "url": "/Portfolio/static/media/pacman1.728a84e9.webp"
+  },
+  {
+    "revision": "16bfd18ab31e3d11c52a8df7bf8b5781",
+    "url": "/Portfolio/static/media/plane.16bfd18a.svg"
+  },
+  {
+    "revision": "de37988dc7123c326841f9dce6b51728",
+    "url": "/Portfolio/static/media/poker-thumbnail.de37988d.webp"
+  },
+  {
+    "revision": "400adc4e45cb4104892b274f1941100f",
+    "url": "/Portfolio/static/media/poker1.400adc4e.webp"
+  },
+  {
+    "revision": "a0c52958a30118f94b41ab47ec2b6f85",
+    "url": "/Portfolio/static/media/project-splash.a0c52958.svg"
+  },
+  {
+    "revision": "059f2be4881c928c4e24fdf925cc0acc",
+    "url": "/Portfolio/static/media/react.059f2be4.png"
+  },
+  {
+    "revision": "c86fcceba0e741b5ec8356bc2df64db7",
+    "url": "/Portfolio/static/media/redux.c86fcceb.png"
+  },
+  {
+    "revision": "4c5c046e314ed321b9e261ec4d3a63d7",
+    "url": "/Portfolio/static/media/wave.4c5c046e.svg"
+  },
+  {
+    "revision": "2480f36ef15bb478506918e85cf9eb6a",
+    "url": "/Portfolio/static/media/weather-thumbnail.2480f36e.webp"
+  },
+  {
+    "revision": "0c337ce84060e5ec1964e2394943a50a",
+    "url": "/Portfolio/static/media/weather1.0c337ce8.webp"
+  },
+  {
+    "revision": "44af981756275ab5ccc3b71ddbecd03b",
+    "url": "/Portfolio/static/media/weather2.44af9817.webp"
+  },
+  {
+    "revision": "d17c45ead88e2a7d9cf1690c2a126818",
+    "url": "/Portfolio/static/media/weather3.d17c45ea.webp"
+  },
+  {
+    "revision": "74ea6763bfca5309a4aadf0d82f1e330",
+    "url": "/Portfolio/static/media/weather4.74ea6763.webp"
+  }
+]);
