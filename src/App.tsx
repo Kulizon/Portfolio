ea81756fafac4bdf1e5822d09bcb1feb5045e83a
kulix <kulixon261@gmail.com>
2023-10-26 12:20:55 +0200

    update path

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
     <div className="app">
       <Routes>
         <Route
-          path="/"
+          path="/Portfolio"
           element={
             <>
               <Menu></Menu>
